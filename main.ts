1ac738c9e94039ba8248e206fd0d676841ce18fb
Justinbarry188 <79929356+Justinbarry188@users.noreply.github.com>
2021-03-26 12:51:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,95 @@
 input.onButtonPressed(Button.A, function () {
-	
+    for (let index = 0; index <= 3; index++) {
+        for (let index = 0; index <= 1; index++) {
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            basic.pause(5000)
+            basic.clearScreen()
+            if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+                pins.digitalWritePin(DigitalPin.P1, 1)
+                pins.digitalWritePin(DigitalPin.P0, 0)
+            }
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            basic.pause(2000)
+            basic.clearScreen()
+            if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                pins.digitalWritePin(DigitalPin.P1, 0)
+            }
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(5000)
+            basic.clearScreen()
+        }
+        for (let index = 0; index <= 9; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # . # # #
+                . # . . .
+                . . # . .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # . # # #
+                . # . . .
+                . . # . .
+                `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    basic.pause(2000)
-    basic.clearScreen()
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    basic.pause(2000)
-    basic.clearScreen()
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    for (let index = 0; index <= 9; index++) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.pause(5000)
-    basic.clearScreen()
-    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-    }
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(5000)
-    basic.clearScreen()
-    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-    }
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(5000)
-    basic.clearScreen()
+	
 })
