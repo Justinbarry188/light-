91764e1d583b6a19edbafab5222787cef3cf5b8a
Justinbarry188 <79929356+Justinbarry188@users.noreply.github.com>
2021-03-30 07:32:15 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index <= 3; index++) {
-        for (let index = 0; index <= 1; index++) {
+        for (let index = 0; index <= 0; index++) {
             pins.digitalWritePin(DigitalPin.P2, 0)
             pins.digitalWritePin(DigitalPin.P0, 1)
             basic.pause(5000)
@@ -55,41 +55,173 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index <= 99; index++) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(500)
+    }
+})
 input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index <= 9; index++) {
+    for (let index = 0; index <= 0; index++) {
+        for (let index = 0; index <= 18; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+            music.playTone(698, music.beat(BeatFraction.Whole))
+        }
         basic.showLeds(`
-            . . # . .
-            . # # # .
+            # . . . #
+            . # . # .
             . . # . .
             . # . # .
-            . . . . .
+            # . . . #
             `)
-        basic.pause(100)
+        basic.pause(15)
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            # . . # .
+            # . # . #
+            # . # . #
+            # . # . #
+            # . . # .
             `)
-        basic.pause(100)
+        music.playTone(698, music.beat(BeatFraction.Whole))
         basic.showLeds(`
-            . . # . .
-            . # # # .
+            # . . . #
+            . # . # .
             . . # . .
             . # . # .
-            . . . . .
+            # . . . #
             `)
-        basic.pause(100)
+        basic.showNumber(9)
+        music.playTone(698, music.beat(BeatFraction.Whole))
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(8)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(7)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(6)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(5)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(4)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(3)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(2)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showNumber(1)
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
             `)
     }
 })
-basic.forever(function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index <= 99; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(500)
+    }
 })
